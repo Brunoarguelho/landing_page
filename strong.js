4681be685b33c08e1e29a2231340c76e6cd94f6d
Bruno A. flauzino <bruno1995jf@gmail.com>
2023-12-06 11:43:52 -0300

    projeto terminado

--- strong.js
+++ strong.js
@@ -5,7 +5,7 @@ var seta2 = document.getElementById('seta2');
 var audio = document.getElementById('Audio');
 seta1.addEventListener('click', function() {
     video.style.height = '250px';
-    video.play(); 
+    video.play(); //ja começa video
     seta1.style.display = 'none'; // esconde a seta para cima
     seta2.style.display = 'block'; // mostra a seta para baixo
     window.scrollTo(0, 0); // move a página para o topo
